09ee46949aad5a5ea13514ab2f26ba2d62861ff5
aycan <aycancotoy@gmail.com>
2021-11-09 17:01:38 +0300

    Coin.js oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,30 @@ import "./App.css";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  
+
   useEffect(() => {
     axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     )
     .then(res => {
       setCoins(res.data);
-      console.log(res.data);
-    })
-  });
+    }).catch(error => alert('Hatalı api Linki'))
+  }, []);
 
   return (
-    <div className="App">
-      <h1>API</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">
+          Search a Currency
+        </h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
+
     </div>
+
   );
 }
 
