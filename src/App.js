3a85ee10e5ee7627aae1af3b3562b107c238e0fa
aycan <aycancotoy@gmail.com>
2021-11-09 16:37:06 +0300

    Coingecko'dan veriler alındı.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    )
+    .then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    })
+  });
+
   return (
     <div className="App">
       <h1>API</h1>
